568af2bd40253ff98f7f9124b061fbdd7304b1c6
nolythedev <nolychigwedere@hotmail.co.uk>
2024-02-20 16:53:47 +0000

    JS functionality started

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,58 @@ console.log('Script is working');
 
 const addBtn = $('#add');
 const assignBtn = $('#assign');
+const userInput = $('#input');
+const idNum = 0;
+const tasks = [];
 
-addBtn.on("click", function() {
-   console.log('clicked');
-
+if (tasks.length <= 0) {
     $('.container-content').append(
-        `<ul>
-        <li>
-            <div class="content">
-                <div class="details">
-                    <button><i class="iconoir-xmark-circle"></i></button>
-                    <p id="task">Hello</p>
-                </div>
-                <div class="profiles">
-                    <i class="iconoir-xmark-circle"></i>
-                </div>
-            </div>
-        </li>
-    </ul>`
+        `<p class="empty-content">No tasks at the moment</p>`
     );
 
-  } );
+}
 
-assignBtn.on("click", function() {
-   console.log('clicked');
-  } );
+addBtn.on("click", function () {
+    // console.log('clicked');
+
+    let task = { name:userInput.val().trim(), age: 25 }; 
+    tasks.push(task); 
+    console.log(tasks);
+    
+        userInput.val('');
+    // }
+});
+
+assignBtn.on("click", function () {
+    console.log('clicked');
+});
+
+// function displayTasks(params) {
+
+//     for (let i = 0; i < tasks.length; i++) {
+//         const element = tasks[i];
+
+//         console.log(element);
+
+//     //          $('.container-content').append(
+//     //         `<ul>
+//     //     <li>
+//     //         <div class="content">
+//     //             <div class="details">
+//     //                 <button><i class="iconoir-xmark-circle"></i></button>
+//     //                 <p id="task" data-id="">${tasks[element].name}</p>
+//     //             </div>
+//     //             <div class="profiles">
+//     //                 <i class="iconoir-xmark-circle"></i>
+//     //             </div>
+//     //         </div>
+//     //     </li>
+//     // </ul>`
+//     //     );
+        
+//     }
+//         // if (userInput.val().trim() != '') {
+   
+// }
+
+// displayTasks();
